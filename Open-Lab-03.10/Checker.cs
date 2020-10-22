5ddc72155705eb15345c73204ad44be8263f8d28
Lukáš Gašpierik <Luk Gapierik@LAPTOP-GBRVOBEH>
2020-10-22 22:23:03 +0200

    Hotovo!

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_03._10
 {
     public class Checker
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int times = str.Count(f => (f == letter));
+            return times;
         }
     }
 }
